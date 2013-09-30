5f23ce38b748dd04787247944861f0687fe9f42f
AmusableBman <AmusableBman@gmail.com>
2013-09-30 08:29:33 -0400

    Create javascript.js
    create the javascript file, add functions for the buttons

---
+++ javascript.js
@@ -0,0 +1,29 @@
+function homeActive()
+{
+  var pageInUse = document.getElementById("button1");
+  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+}
+
+function aboutMeActive()
+{
+  var pageInUse = document.getElementById("button2");
+  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+}
+
+function projectsActive()
+{
+  var pageInUse = document.getElementById("button3");
+  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+}
+
+function servicesActive()
+{
+  var pageInUse = document.getElementById("button4");
+  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+}
+
+function contactMeActive()
+{
+  var pageInUse = document.getElementById("button5");
+  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+}
