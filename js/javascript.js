21c5e0dd4deb1d84d37ecccac3560d988023d0d6
AmusableBman <AmusableBman@gmail.com>
2013-09-30 16:33:22 -0400

    trying to center the damn buttons, and changes to jscript

--- js/javascript.js
+++ js/javascript.js
@@ -7,31 +7,31 @@ var button5 = document.getElementById("button5");
 function homeActive()
 {
   var pageInUse = document.getElementById("button1");
-  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+  pageInUse.setAttribute("style","background-color: yellow;");
 }
 
 function aboutMeActive()
 {
   var pageInUse = document.getElementById("button2");
-  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+  pageInUse.setAttribute("style","background-color: yellow;");
 }
 
 function projectsActive()
 {
   var pageInUse = document.getElementById("button3");
-  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+  pageInUse.setAttribute("style","background-color: yellow;");
 }
 
 function servicesActive()
 {
   var pageInUse = document.getElementById("button4");
-  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+  pageInUse.setAttribute("style","background-color: yellow;");
 }
 
 function contactMeActive()
 {
   var pageInUse = document.getElementById("button5");
-  pageInUse.setAttribute("style","width: 500px; background-color: yellow;");
+  pageInUse.setAttribute("style","background-color: yellow;");
 }
 
 button1.onclick=function(){
